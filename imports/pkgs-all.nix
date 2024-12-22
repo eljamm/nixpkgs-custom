e960cb4717c1903b537406857c3380ec1240477e
eljamm <fedi.jamoussi@protonmail.ch>
2024-12-22 18:26:04 +0100

    feat: init yuzu

--- imports/pkgs-all.nix
+++ imports/pkgs-all.nix
@@ -4,8 +4,15 @@
   perSystem =
     { pkgs, ... }:
     {
-      packages = with pkgs; {
+      packages = with pkgs; rec {
         vocabsieve = libsForQt5.callPackage ../pkgs/vocabsieve/package.nix { };
+
+        # from https://github.com/NixOS/nixpkgs/pull/295587
+        yuzuPackages = callPackage ../pkgs/yuzu { };
+        yuzu = yuzuPackages.mainline;
+        yuzu-ea = yuzuPackages.early-access;
+        yuzu-early-access = yuzuPackages.early-access;
+        yuzu-mainline = yuzuPackages.mainline;
       };
     };
 }
