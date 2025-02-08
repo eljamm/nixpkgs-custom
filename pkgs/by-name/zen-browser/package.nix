22e94a6c4eee85d903c2390f4cc57695813550a7
eljamm <fedi.jamoussi@protonmail.ch>
2025-02-08 10:36:16 +0100

    zen-browser-bin: 1.7.4b -> 1.7.6b

--- pkgs/by-name/zen-browser/package.nix
+++ pkgs/by-name/zen-browser/package.nix
@@ -65,11 +65,11 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "zen-browser-bin";
-  version = "1.7.4b";
+  version = "1.7.6b";
 
   src = fetchzip {
-    url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.bz2";
-    hash = "sha256-hiGb2oyAueKa6Xs6BvtEmgPTRlOXvJ5YDseS8kZ1VbQ=";
+    url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.xz";
+    hash = "sha256-qVrwjQ147cRqDMFHz6eS6K/qAqQlmMRORHEYGRSwZqc=";
   };
 
   desktopItems = [
