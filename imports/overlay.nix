8a8f739760472a98475fe4f240622f2b7502e1a4
eljamm <fedi.jamoussi@protonmail.ch>
2025-03-01 06:08:56 +0100

    fix: stack overflow; max-call-depth exceeded

--- imports/overlay.nix
+++ imports/overlay.nix
@@ -17,7 +17,7 @@
       _module.args = {
         pkgs = import inputs.nixpkgs {
           inherit system;
-          overlays = [ inputs.self.overlays.default ];
+          overlays = [ ];
           config = { };
         };
 
