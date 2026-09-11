b0825867e38474b50a87fdb72ea8665664306675
eljamm <fedi.jamoussi@protonmail.ch>
2026-09-11 09:23:47 +0200

    build: switch nixpkgs input to channel; update flake.lock

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Custom Nixpkgs Repo";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.zst";
     flake-utils.url = "github:numtide/flake-utils";
 
     treefmt-nix.url = "github:numtide/treefmt-nix";
