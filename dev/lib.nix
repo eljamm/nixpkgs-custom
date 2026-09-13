2f09f5579be2201c9fef6870317136e78f0cffe9
eljamm <fedi.jamoussi@protonmail.ch>
2026-09-13 11:16:05 +0200

    docs: use doc-style comment for isNewerOrSame function

--- dev/lib.nix
+++ dev/lib.nix
@@ -3,15 +3,17 @@
   ...
 }:
 {
-  # Compares 2 packages and returns `true` if the first is newer than the
-  # second or the same.
-  #
-  # Reference:
-  # *  0: same version
-  # *  1: p1 is newer
-  # * -1: p1 is older
-  #
-  # https://noogle.dev/f/lib/versions/compareVersions
+  /*
+    Compares 2 packages and returns `true` if the first is newer than the
+    second or the same.
+
+    Reference:
+    *  0: same version
+    *  1: p1 is newer
+    * -1: p1 is older
+
+    https://noogle.dev/f/lib/versions/compareVersions
+  */
   isNewerOrSame = p1: p2: (lib.strings.compareVersions p1.version p2.version) != -1;
 
   # Pass condition with custom message and warn if it's true.
