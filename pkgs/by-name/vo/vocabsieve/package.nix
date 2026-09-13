55b45719a5aa0f2da71b9450660c34d2096c5e17
eljamm <fedi.jamoussi@protonmail.ch>
2026-09-13 11:25:23 +0200

    fix(vocabsieve): remove gst-vaapi
    See:
    https://github.com/NixOS/nixpkgs/commit/b812bc2a4a1597b73bc9a5422318fe6bcc9bde34

--- pkgs/by-name/vo/vocabsieve/package.nix
+++ pkgs/by-name/vo/vocabsieve/package.nix
@@ -33,7 +33,7 @@ python3Packages.buildPythonApplication rec {
     gst_all_1.gst-libav
     gst_all_1.gst-plugins-base
     gst_all_1.gst-plugins-good
-    gst_all_1.gst-vaapi
+    gst_all_1.gst-plugins-bad
     gst_all_1.gstreamer
   ];
 
